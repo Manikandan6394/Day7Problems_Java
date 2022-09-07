4238ba5bfe62db9f481a3e38e8a22e19f3187cc8
Manikandan6394 <manikandan.ramesh6394@gmail.com>
2022-09-07 17:14:20 +0530

    Started the random dice program

---
+++ src/UC1.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+/**
+ * 
+ */
+
+/**
+ * @author My self
+ *
+ */
+public class UC1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int n = 0;
+		Random r = new Random();
+		n=r.nextInt(7);
+		 for (int i = 0; i <6; i++) {
+		 int start = 0;
+		 
+
+	}
+	}
+}
